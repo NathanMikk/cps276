72d1112191151b986c6ba099011be385e7faf381
Nathan Mikkelson <nmikkelson_wccnet@MININT-K781HSH.labs.is.wccnet.org>
2023-03-30 15:08:16 -0400

    updates

---
+++ assignments/starter_scripts/php/addName.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once "../starter_scripts/classes/Pdo_methods.php";
+
+$data = json_decode($_POST[$data]);
+$name = $data->name;
+$nameR = esplode(" ", $name);
+$name = "{$nameR[1]}, {$nameR[0]}";
+
+$pdo = new PdoMethods();
+
+$sql = "INSERT INTO names (name) VALUES (:name)";
+
+$bindings = [
+    [":name", $name, "str"],
+];
+
+$records = $pdo->otherBinded($sql, $bindings);
+if($records === "error"){
+    $response = (object)[
+        "masterstatus" => "error",
+        "msg" => "could not add name to database"
+    ];
+    echo json_encode($response);
+} else{
+    $response = (object)[
+        "masterstatus" => "success",
+        "msg" => "$name added"
+    ];
+    echo json_encode($response);
+}
+
+?>
---
+++ assignments/starter_scripts/php/displayNames.php
@@ -0,0 +1,29 @@
+<?php
+require_once "../starter_scripts/classes/Pdo_methods.php";
+
+$pdo = new PdoMethods();
+
+$sql = "SELECT * FROM names order by name;";
+
+$records = $pdo->selectNotBinded($sql);
+
+if($records == "error"){
+    $response = (object)[
+        "masterstatus" => "error",
+        "msg" => "Could not get names."
+    ];
+    echo json_encode($response);
+} else{
+    $list = "";
+    foreach($records as $name)
+    {
+        $list .= "<p>" .implode($name). "</p>";
+    }
+    response = (object)[
+        "masterstatus" => "success",
+        "names" => $list
+    ];
+    echo json_encode($response);
+}
+
+?>
